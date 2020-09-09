fbae778b1d8f001369b088386303c1a93201d656
linnil1 <linnil1.886@gmail.com>
2020-09-09 14:58:33 +0800

    Add tango run script

--- run.sh
+++ run.sh
@@ -1,28 +1,42 @@
+echo "DO NOT RUN THIS, This is just a buliding note"
+exit
+#
 # Before
+#
+
+# clone tango
+git clone https://github.com/autolab/Tango.git
+cp Tango/requirments.txt build_tango
+
 # clone Autolab
 git clone https://github.com/autolab/Autolab.git
 cd Autolab
 git checkout a4563ea24d32d3efc9644e758b32fe4cbc138e71
 cp config/autogradeConfig.rb.template config/autogradeConfig.rb
-
-# Edit this three files(Run maually)
-cp ../autolab/school.yml config/school.yml
-cp ../autolab/database.yml config/database.yml
-cp ../autolab/devise.rb config/initializers/devise.rb
 cd ..
 
-# build dockerfile
-cd build
+# build autolab
+cd build_autolab
 docker build . -f Dockerfile -t linnil1/autolab
 cd ..
 
-# Prepare data for mountint
-cp -r Autolab/db autolab/
-mkdir -p Autolab/tmp Autolab/courses Autolab/assessmentConfig Autolab/courseConfig Autolab/gradebooks Autolab/log
+# build tango
+cp Tango/requirments.txt build_tango
+cd build_tango
+docker build . -t linnil1/tango
+cd ..
 
 # Edit manullay
-cat docker-compose.yml
 cat web/nginx.conf
+cp tango/config.py Tango/
+cat docker-compose.yml
+cp autolab/school.yml Autolab/config/school.yml
+cp autolab/database.yml Autolab/config/database.yml
+cp autolab/devise.rb Autolab/config/initializers/devise.rb
+
+# Prepare data for mounting
+cp -r Autolab/db autolab/
+mkdir -p Autolab/tmp Autolab/courses Autolab/assessmentConfig Autolab/courseConfig Autolab/gradebooks Autolab/log
 
 # Main
 docker-compose up -d
