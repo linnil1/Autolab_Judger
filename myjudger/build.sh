cd6c40058c79713e54caf1bef7adb064096d3dd7
linnil1 <linnil1.886@gmail.com>
2020-11-25 16:26:24 +0800

    Remove myjudger, i will add final template in the end

--- myjudger/build.sh
+++
@@ -1,4 +0,0 @@
-chown 1005:1005 autograde -R
-chmod 700 autograde -R
-chmod 705 autograde autograde/py_grade.py autograde/java_grade.java autograde/*.jar
-tar -cf autograde.tar autograde/
--- myjudger/transfer.sh
+++
@@ -1,2 +0,0 @@
-sudo cp autograde-Makefile ../autolab/courses/109-1-PDSA/hw00/
-sudo cp autograde.tar ../autolab/courses/109-1-PDSA/hw00/
